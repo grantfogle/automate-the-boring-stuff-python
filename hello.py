1fbf8a7a01b26004f6fb9304a5ec239ed71e1941
grantfogle <grantjfogle@gmail.com>
2022-10-03 22:47:23 -0600

    adding for loop

--- hello.py
+++ hello.py
@@ -1,19 +1,33 @@
-password = 'swordfisssh'
+# password = 'swordfisssh'
 
-if password == 'swordfish':
-    print('Access granted')
-elif password == 'swordfissh':
-    print('close but no cigar')
-else:
-    print('wrong password')
+# if password == 'swordfish':
+#     print('Access granted')
+# elif password == 'swordfissh':
+#     print('close but no cigar')
+# else:
+#     print('wrong password')
 
-spam = 0
-while spam < 5:
-    print('Hello world')
-    spam = spam + 1
+# spam = 0
+# while spam < 5:
+#     print('Hello world')
+#     spam = spam + 1
 
-name = ''
-while name != 'your name':
-    print('please type your name')
-    name = input()
-print('Thank you')
+# name = ''
+# while name != 'your name':
+#     print('please type your name')
+#     name = input()
+# print('Thank you')
+
+for i in range(5):
+    print ('Grant five times ' + str(i))
+
+total = 0
+for num in range(101):
+    total = total + num
+
+print(total)
+
+i = 0
+while i <= 5:
+    print (str(i))
+    i = i + 1
