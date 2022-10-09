72eca99cfa624522bd6ddf849f34b8d86ccc8194
grantfogle <grantjfogle@gmail.com>
2022-10-09 14:21:07 -0700

    adding strings formatting

---
+++ strings.py
@@ -0,0 +1,23 @@
+print('Hello there MR. Bob\'s Dad\nHow are you?\n\tI\'m fine')
+
+romeoAndJuliet = """Romeo and Juliet is a tragedy 
+written by William Shakespeare 
+early in his career about two 
+young Italian star-crossed lovers 
+whose deaths ultimately reconcile their feuding families. 
+It was among Shakespeare's most 
+popular plays during his lifetime 
+and, along with Hamlet, is one of 
+
+his most frequently"""
+# print(romeoAndJuliet, len(romeoAndJuliet))
+
+spam = 'Hello World!'
+print(spam.upper())
+print(spam.lower())
+name = 'John'
+time = '8pm'
+thing = 'dip'
+
+longStr = 'Hello %s, you are invited to a party at %s, \nplease bring %s.' % (name, time, thing)
+print(longStr)
